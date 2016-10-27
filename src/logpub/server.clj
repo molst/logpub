2311cdf987d9328fe091bff6f658bf94731e541d
molst <molst@yahoo.com>
2016-10-27 22:56:21 +0200

    list all log files of right age

--- src/logpub/server.clj
+++ src/logpub/server.clj
@@ -2,10 +2,34 @@
   (:gen-class)
   (:require
    [compojure.core :refer :all]
-   [ring.adapter.jetty :as r]))
+   [ring.adapter.jetty :as r]
+   [clojure.java.io :as io]
+   [clojure.pprint :refer [pprint]]))
+
+(defn newer-than [max-age file]
+  (and (not (.isDirectory file))
+       (<= (- (System/currentTimeMillis)
+              (.lastModified file))
+           max-age)))
+
+(defn contained-files-newer-than [dir max-age]
+  (remove (fn [file]
+            (not (newer-than max-age file)))
+          (file-seq dir)))
+
+(def week-in-millis 604800000)
+
+(defn filter-dirs
+  [& base-uris]
+  (for [base-uri base-uris
+        file (contained-files-newer-than (io/file base-uri) week-in-millis)]
+    (.getPath file)))
 
 (defroutes router
-  (GET "/" [] "this is not a list of options"))
+  (GET "/logs" [] (-> "/usr/hostmirror/var/log"
+                      filter-dirs
+                      pprint
+                      with-out-str)))
 
 (defn -main []
   (r/run-jetty #'router {:port 4442}))
